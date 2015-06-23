e573463deb636f70f6a9a82cdf4b2ca27462e38e
Neil Heinrich <nheinrich@gmail.com>
2015-06-23 17:20:25 -0600

    does this help?

--- lib/babar/session.rb
+++ lib/babar/session.rb
@@ -13,8 +13,12 @@ module Babar::Session
   # * +access_token+ - A valid access token see Naviance::AccessToken#get
 
   def self.start
-    app_id = ENV["elephant_talk_id"]
-    app_key = ENV["elephant_talk_key"]
+    # app_id = ENV["elephant_talk_id"]
+    # app_key = ENV["elephant_talk_key"]
+    raise "WHAT THE FUCKING FUCK"
+
+    app_id = "hunt"
+    app_key = "3c78a911-3453-4113-860c-99ca2aa14769"
 
     url = "#{Babar.base_url}/CreateSecureSession?password=#{app_key}&promocodeName=#{app_id}"
 
@@ -23,6 +27,7 @@ module Babar::Session
     if response.code == 200
       @id = response.parsed_response
     else
+      raise "Shit is fucked"
       Babar.error(response)
     end
   end
